8db05c6eecf6320db16dda619978574776613997
Sahan <83448005+RandikaVS@users.noreply.github.com>
2025-02-18 15:49:35 +0530

    Revert "feat: drawer with tab navigation partialy complete (#2)" (#3)
    This reverts commit 3a6632c4559dd7e8c9f14319082236b196ad6f95.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,21 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { SplashScreen, Stack, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import "react-native-reanimated";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { AuthProvider } from "../context/auth/index";
-import { useAuthContext } from "../context/hooks/use-auth-context";
-import Toast from "react-native-toast-message";
-import LoadingScreen from "@/components/LoadingScreen";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import 'react-native-reanimated';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { AuthProvider } from '../context/auth/index';
+import { useAuthContext } from '../context/hooks/use-auth-context';
+import Toast  from 'react-native-toast-message';
+import LoadingScreen from '@/components/LoadingScreen';
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [isReady, setIsReady] = useState(false);
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   const router = useRouter();
@@ -34,7 +30,7 @@ export default function RootLayout() {
   return (
     <AuthProvider>
       <AuthLayout isReady={isReady} router={router} colorScheme={colorScheme} />
-      <Toast />
+      <Toast/>
     </AuthProvider>
   );
 }
@@ -45,35 +41,29 @@ interface AuthLayoutProps {
   colorScheme: any;
 }
 
-const AuthLayout: React.FC<AuthLayoutProps> = ({
-  isReady,
-  router,
-  colorScheme,
-}) => {
+const AuthLayout: React.FC<AuthLayoutProps> = ({ isReady, router, colorScheme }) => {
   const { user, loading } = useAuthContext();
 
   useEffect(() => {
     if (isReady && !loading) {
-      if (true) {
-        router.replace("/(drawer)");
+      if (user) {
+        router.replace('/(tabs)');
       } else {
-        router.replace("/(auth)");
+        router.replace('/(auth)');
       }
     }
-  }, [isReady, user, loading, router]);
+  }, [isReady, user,loading,router]);
 
   if (!isReady && loading) {
-    return <LoadingScreen />;
+    return <LoadingScreen/>
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(auth)" />
-
-        <Stack.Screen name="(drawer)" />
-
-        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+        <Stack.Screen name='(auth)' />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
   );
